5c893a933281d2860047b8059697cc5f6111e3dd
Shyam Kumar Sahu <100072621+gitsks@users.noreply.github.com>
2022-05-23 19:44:51 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World from GitHub cron2!");
+    System.out.println("Hello World from GitHub by webhook!");
     }
 }
